0ded1f9f25d784c704e19ddf5018fadf2de674a6
Marko Skorup <mskorup@gmail.com>
2022-06-07 12:48:00 +0200

    zadatak 3, ASCII zivotinja

---
+++ ASCII/src/zadatak03/Zivotinja.java
@@ -0,0 +1,14 @@
+package zadatak03;
+
+public class Zivotinja {
+
+	public static void main(String[] args) {
+		System.out.println("   / \\__\r\n"
+				+ "  (    @\\___\r\n"
+				+ "  /         O\r\n"
+				+ " /   (_____/\r\n"
+				+ "/_____/   U");
+
+	}
+
+}
