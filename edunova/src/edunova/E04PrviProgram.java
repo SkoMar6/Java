fc6affdfe42e3c2aa638cb7afb09f8e3bb9664e9
Marko Skorup <mskorup@gmail.com>
2022-06-12 17:43:21 +0200

    Java prvi program

---
+++ edunova/src/edunova/E04PrviProgram.java
@@ -0,0 +1,20 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class E04PrviProgram {
+
+	public static void main(String[] args) {
+		// Prje korištenja JOptionPane
+		// u module info treba dodati
+		// requires java.desktop;
+
+		String unosOdKorisnika = JOptionPane.showInputDialog("Unesi broj");
+		
+		int b = Integer.parseInt(unosOdKorisnika);
+		
+		System.out.println(b);
+	}
+
+}
+ 
