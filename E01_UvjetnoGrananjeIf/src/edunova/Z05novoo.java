88b697d25bbc2dc062c48e8b05e2bb59f3132f5e
Marko Skorup <mskorup@gmail.com>
2022-06-20 13:28:22 +0200

    Z05ponovo

---
+++ E01_UvjetnoGrananjeIf/src/edunova/Z05novoo.java
@@ -0,0 +1,39 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z05novoo {
+
+	public static void main(String[] args) {
+		// program unosi broj izmeðu 1 i 999 u sluèaju da je broj izvan tog raspona
+		// ispisati grešku i prekinuti program
+		// program ispisuje prvu znamenku upisanog broja
+		
+		
+		int broj = Integer.parseInt(JOptionPane.showInputDialog("unesi broj izmeðu 1 i 999"));
+		
+		if (broj<1 || broj>999) {
+			System.out.println("greška");
+		}
+		
+		if (broj<10) {
+			System.out.println(broj);
+		}else if (broj>10 && broj <100) {
+			System.out.println(broj/10);
+		}else {
+			System.out.println(broj/100);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
