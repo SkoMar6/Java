554d32568b70f5d46ed0fa581bba25fe423caca3
Marko Skorup <mskorup@gmail.com>
2022-06-20 16:48:14 +0200

    Update Z06.java

--- E01_UvjetnoGrananjeIf/src/edunova/Z06.java
+++ E01_UvjetnoGrananjeIf/src/edunova/Z06.java
@@ -5,17 +5,18 @@ import javax.swing.JOptionPane;
 public class Z06 {
 
 	public static void main(String[] args) {
-		// program unosi 2 broja, ako su parni ispisuje njihov broj inaèe ispisuje njihovu razliku
-		
+		// program unosi 2 broja, ako su parni ispisuje njihov broj inaèe ispisuje
+		// njihovu razliku
+
 		int prviBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi broj"));
 		int drugiBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi broj"));
-		
-		int zbroj = prviBroj+drugiBroj;
-		int razlika =prviBroj-drugiBroj;
-		
-		if (prviBroj%2==0 && drugiBroj%2==0) {
-		System.out.println(zbroj);
-		}else  {
+
+		int zbroj = prviBroj + drugiBroj;
+		int razlika = prviBroj - drugiBroj;
+
+		if (prviBroj % 2 == 0 && drugiBroj % 2 == 0) {
+			System.out.println(zbroj);
+		} else {
 			System.out.println(razlika);
 		}
 
