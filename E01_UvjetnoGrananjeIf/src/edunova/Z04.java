191085c67dd5465d8f1d14fb74d31e308592c7d3
Marko Skorup <mskorup@gmail.com>
2022-06-20 12:49:08 +0200

    Z05

--- E01_UvjetnoGrananjeIf/src/edunova/Z04.java
+++ E01_UvjetnoGrananjeIf/src/edunova/Z04.java
@@ -5,22 +5,19 @@ import javax.swing.JOptionPane;
 public class Z04 {
 
 	public static void main(String[] args) {
-		//za unesena 3 cijela broja program ispisuje najveæi
-		
-		int prviBroj= Integer.parseInt(JOptionPane.showInputDialog("unesi cijeli broj"));
+		// za unesena 3 cijela broja program ispisuje najveæi
+
+		int prviBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi cijeli broj"));
 		int drugiBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi cijeli broj"));
-		int treciBroj= Integer.parseInt(JOptionPane.showInputDialog("unesi cijeli broj"));
-		
-		if ((prviBroj>drugiBroj) && (prviBroj>treciBroj)){
+		int treciBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi cijeli broj"));
+
+		if ((prviBroj > drugiBroj) && (prviBroj > treciBroj)) {
 			System.out.println(prviBroj);
-		}else if ((drugiBroj>prviBroj) && (drugiBroj>treciBroj)) {
+		} else if ((drugiBroj > prviBroj) && (drugiBroj > treciBroj)) {
 			System.out.println(drugiBroj);
-		}else {
+		} else {
 			System.out.println(treciBroj);
 		}
-		
-		
-		
 
 	}
 
---
+++ E01_UvjetnoGrananjeIf/src/edunova/Z05.java
@@ -0,0 +1,28 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z05 {
+
+	public static void main(String[] args) {
+		// program unosi broj izmeðu 1 i 999 u sluèaju da je broj izvan tog raspona
+		// ispisati grešku i prekinuti program
+		// program ispisuje prvu znamenku upisanog broja
+
+		int broj = Integer.parseInt(JOptionPane.showInputDialog("unesi broj izmeðu 1 i 999"));
+
+		if (broj < 1 || broj > 999) {
+			System.out.println("broj izvan raspona 1 i 999");
+		} else {
+			if (broj < 10) {
+				System.out.println(broj);
+			} else if (broj >= 10 && broj < 100) {
+				System.out.println(broj / 10);
+			}else {
+				System.out.println(broj/100);
+			}
+		}
+
+	}
+
+}
