17c46a84ffbad56caa4dce93b0bdfdee9bad9911
Marko Skorup <mskorup@gmail.com>
2022-06-20 11:37:03 +0200

    Z03

--- E01_UvjetnoGrananjeIf/src/edunova/Z02.java
+++ E01_UvjetnoGrananjeIf/src/edunova/Z02.java
@@ -1,22 +1,20 @@
 package edunova;
+
 import javax.swing.JOptionPane;
+
 public class Z02 {
 
 	public static void main(String[] args) {
 		// za upisani cijeli broj
 		// program ispisuje da li je parni ili ne
-		
-	int broj= Integer.parseInt(
-			JOptionPane.showInputDialog("unesi cijeli broj")
-			);	
-		
-		
-		if (broj%2==0) {
+
+		int broj = Integer.parseInt(JOptionPane.showInputDialog("unesi cijeli broj"));
+
+		if (broj % 2 == 0) {
 			System.out.println("parni broj");
-		}else {
+		} else {
 			System.out.println("neparni broj");
 		}
-		
 
 	}
 
---
+++ E01_UvjetnoGrananjeIf/src/edunova/Z03.java
@@ -0,0 +1,28 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z03 {
+
+	public static void main(String[] args) {
+		//za unesena 2 cijela broj program ispisuje najveæi
+		
+		
+		int prviBroj=Integer.parseInt(
+				JOptionPane.showInputDialog("unesi cijeli broj")
+				);
+		
+		int drugiBroj=Integer.parseInt(
+				JOptionPane.showInputDialog("unesi cijeli broj")
+				);
+		
+		if (prviBroj>drugiBroj) {
+			System.out.println(prviBroj);
+		}else {
+			System.out.println(drugiBroj);
+		}
+		
+
+	}
+
+}
