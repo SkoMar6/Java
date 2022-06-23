d3d49ae0663ec9eba41b1bcdbb2c8b51b04882f9
Marko Skorup <mskorup@gmail.com>
2022-06-23 14:34:19 +0200

    E03 Višedimenzionalni nizovi

---
+++ Edunova04/src/edunova/E03ViseDimenzionalniNizovi.java
@@ -0,0 +1,34 @@
+package edunova;
+
+public class E03ViseDimenzionalniNizovi {
+
+	public static void main(String[] args) {
+		// dvodimenzionalni niz
+		//tablica
+		 int [][] matrica = new int [10][10];
+		 
+		 //u skroz donjem desnom kutu postaviti broj 7
+		 
+		 matrica [9][9]=7;
+		 
+		
+			int[][] tablica = {
+					{1,0,0},
+					{0,1,0},
+					{0,0,1}
+			};
+		
+		
+		
+			// trodimenzionalni niz: 
+			int[][][] kocka = new int[10][10][10];
+			
+			
+			// èetvero dimenzionalni niz: vrijeme je 4. dimenzija
+			
+			// java podržava do 15 dimenzija
+			
+		
+		}
+
+}
