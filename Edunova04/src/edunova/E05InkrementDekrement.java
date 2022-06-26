2ee824a85407ee28bd489129f8bfde3c9d0ce012
Marko Skorup <mskorup@gmail.com>
2022-06-26 10:57:09 +0200

    E05 Inkrement Dekrement

---
+++ Edunova04/src/edunova/E05InkrementDekrement.java
@@ -0,0 +1,29 @@
+package edunova;
+
+public class E05InkrementDekrement {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		
+		System.out.println(i++); // 1  Varijabla se prvo koristi pa uveæava
+		System.out.println(i); // 2
+		System.out.println(++i);  //3 prvo se uveæa pa koristi
+		
+		System.out.println(i--); //3 umanji za 1 pa je vrijednost 2
+		System.out.println(--i); //umanji za 1 pa ispiše 
+		
+		int t=2, k=7;
+		
+		t+= ++t -k--; //3-7 = -4, t= -2,  k= 6
+		
+		System.out.println("t=" + t + ", k=" +k );
+		
+		k=t++; //k =-2, t= -1
+		System.out.println("t="+t + ", k="+k);
+		System.out.println(k+t);
+		
+
+	}
+
+}
