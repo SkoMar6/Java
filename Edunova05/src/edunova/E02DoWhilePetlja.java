808b53461a917e73d9dae6093b0ffdbb77b4da7d
Marko Skorup <mskorup@gmail.com>
2022-07-01 18:40:29 +0200

    E02DoWhilePetlja

---
+++ Edunova05/src/edunova/E02DoWhilePetlja.java
@@ -0,0 +1,29 @@
+package edunova;
+
+public class E02DoWhilePetlja {
+
+	public static void main(String[] args) {
+		// Osigurava minimalno jednu iteraciju petlje
+		// uvjet izlaska petlje je na kraju
+		
+		
+		int i=10;
+		do {
+			System.out.println("Osijek");
+			
+		}while (i>10);
+		
+		
+		// sve ostalo što vrijedi za for i while 
+		// vrijedi i za do while
+		
+		
+		//beskonaèna do while
+		
+		do {
+			break;
+		}while (true);
+
+	}
+
+}
