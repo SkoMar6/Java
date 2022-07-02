f98174da7127fd4d731b4d7c6405b53b1fed3519
Marko Skorup <mskorup@gmail.com>
2022-07-02 12:38:01 +0200

    E04 ForEachPetlja

---
+++ Edunova05/src/edunova/E04ForEachPetlja.java
@@ -0,0 +1,33 @@
+package edunova;
+
+public class E04ForEachPetlja {
+
+	public static void main(String[] args) {
+		
+		
+		int [] niz = {1,5,4,2,1,-1 ,2,4};
+		
+		//ispiši svaki element niza jedno ispod drugog
+		// s for petljom
+		
+		for (int i=0;i<niz.length;i++) {
+			System.out.println(niz[i]);
+		}
+		System.out.println("--------------");
+		// foreach
+		
+		for(int broj:niz) {
+			
+			//nemamo pristup indexu
+			if (broj==4) {
+				continue;
+			}
+			if(broj==-1) {
+				break;
+			}
+			System.out.println(broj);
+		}
+
+	}
+
+}
